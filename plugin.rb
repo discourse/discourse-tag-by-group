cc304fdd48855def5029c0c310f5bb0c833cd3af
Kelv <kelv@discourse.org>
2023-08-25 11:40:13 +0800

    DEV: Add packages & linting and MIT license (#2)
    * DEV: update url in plugin.rb and add packages & linting
    
    * DEV: omit tag_by_group_spec from RSpec/DescribeClass rule
    
    * DEV: pin syntax_tree to main
    
    * DEV: apply syntax_tree changes
    
    * Update package.json with MIT license
    
    * Create MIT license

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: discourse-tag-by-group
 # version 0.1
 # authors: scossar
-# url: https://github.com/discourse-org/discourse-tag-by-group
+# url: https://github.com/discourse/discourse-tag-by-group
 
 enabled_site_setting :tag_by_group_enabled
 
@@ -11,10 +11,10 @@ after_initialize do
   on(:topic_created) do |topic|
     if topic.regular? && topic.user_id > 0
       u = topic.user
-      tag_group_relations = SiteSetting.tag_by_group_tags_for_groups.split('|')
+      tag_group_relations = SiteSetting.tag_by_group_tags_for_groups.split("|")
       user_group_names = u.groups.pluck(:name)
       tag_group_relations.each do |r|
-        relation = r.split(',')
+        relation = r.split(",")
         if user_group_names.include? relation[0]
           tag_name = relation[1]&.strip
           tag = Tag.find_by_name(tag_name) || Tag.create(name: tag_name)
